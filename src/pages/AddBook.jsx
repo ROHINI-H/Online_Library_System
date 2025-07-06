50f658c8fc04c25c270e50b6bee20c94f69be8f8
Rohini <h.rohini19@gmail.com>
2025-07-06 18:02:09 +0530

    Added the favicon and the title

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -18,9 +18,9 @@ function AddBook() {
     // Handles the error message inline
     function validate() {
         const error = {};
-        if(!title.trim()) error.title = 'Title is required';
-        if(!author.trim()) error.author = 'Author is required';
-        if(rating && (rating <1 || rating > 5)) error.rating = 'Rating must be 1 to 5';
+        if (!title.trim()) error.title = 'Title is required';
+        if (!author.trim()) error.author = 'Author is required';
+        if (rating && (rating < 1 || rating > 5)) error.rating = 'Rating must be 1 to 5';
         return error;
     }
 
@@ -30,7 +30,7 @@ function AddBook() {
         setErrors(validate());
 
         //checks for error. If there is error, then stops the form submission
-        if(Object.keys(validate().length > 0)) return;
+        if (Object.keys(validate().length > 0)) return;
 
         dispatch(addBook({ title, author, description, category, rating, popular }));
         navigate('/books');
@@ -40,14 +40,14 @@ function AddBook() {
         <form onSubmit={handleSubmit} className="max-w-xl mx-auto mt-12 p-7 bg-grey rounded shadow space-y-5">
             <h2 className="text-xl font-bold text-amber-900">Add a new Book</h2>
             <div>
-            <input className="w-full border p-1.5" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <input className="w-full border p-1.5" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 {errors.title && <p className="text-red-600 text-sm">{errors.title}</p>}
             </div>
             <div>
-            <input className="w-full border p-1.5" type="text" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                <input className="w-full border p-1.5" type="text" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
                 {errors.author && <p className="text-red-600 text-sm">{errors.author}</p>}
             </div>
-            
+
             <textarea className="w-full border p-1.5" type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
             <select className="w-full border p-1.5" value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option>Self-Help</option>
@@ -56,7 +56,7 @@ function AddBook() {
                 <option>Fiction</option>
             </select>
             <div>
-            <input className="w-full border p-1.5" type="number" placeholder="Rating (1-5)" value={rating} onChange={(e) => setRating(e.target.value)} />
+                <input className="w-full border p-1.5" type="number" placeholder="Rating (1-5)" value={rating} onChange={(e) => setRating(e.target.value)} />
                 {errors.rating && <p className="text-red-600 text-sm">{errors.rating}</p>}
             </div>
             <label className="flex items-center space-x-1.5">
