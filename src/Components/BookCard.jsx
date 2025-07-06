fb23369474a2a97c4d049832803f6dba2ff2d9bd
Rohini <h.rohini19@gmail.com>
2025-07-06 07:47:13 +0530

    Corrected the error and now the book details page is working correctly

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -5,7 +5,7 @@ function BookCard({ book }) {
         <div className="border p-3 rounded shadow hover:shadow-md">
             <h3 className="text-lg font-medium text-pink-600">{book.title}</h3>
             <p className="text-gray-700 pb-3">by {book.author}</p>
-            <Link to={`book/${book.id}`} className="text-emerald-600 hover:text-emerald-800 hover:underline">View Details</Link>
+            <Link to={`/book/${book.id}`} className="text-emerald-600 hover:text-emerald-800 hover:underline">View Details</Link>
         </div>
     )
 }
--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -12,13 +12,14 @@ function BookDetails() {
 
     return (
         <div className="p-7">
-            <h2 className="text-2xl font-bold">{book.title}</h2>
+            <h2 className="text-2xl font-bold text-teal-600">{book.title}</h2>
             <p className="text-lg mb-2">by {book.author}</p>
+            <img className="mb-2" src={book.coverImage} alt="" width="200px" />
             <p className="mb-2">{book.description}</p>
             <p className="mb-2">Category: {book.category}</p>
             <p className="mb-2">Rating: {book.rating || 'N/A'}</p>
             <p className="mb-2">Published Date: {book.publishedDate}</p>
-            <button onClick={() => navigate(-1)} className="text-blue-500 hover:underline">← Back to Browse</button>
+            <button onClick={() => navigate(-1)} className="text-blue-600 hover:underline">← Back to Browse</button>
         </div>
     )
 }
